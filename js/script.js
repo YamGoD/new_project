de9a6652c0690d49bb1ab8b37ef708312c594a4a
YamGod <backtrack5r1991@gmail.com>
2021-03-12 09:33:24 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+let number = 4;
+
+// console.log( 'string' * 9 );
+
+const object = {
+    name: "John",
+    age: 50,
+    isMarried: true
+};
+
+// console.log(object.name);
+console.log(object["name"]);
+let array = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(array[1]);
---
+++ js/script2.js
@@ -0,0 +1,13 @@
+'use strict';
+// alert('Hello');
+// const result = confirm('are you here?');
+// const result = prompt('Вам есть 18?','да');
+// console.log(typeof(result));
+
+const answers = [];
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
+
+console.log(typeof(answers));
+console.log(typeof(null));
